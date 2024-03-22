a1f05f80f4903162254cb0eeb4ae50b2963935b6
gerpi <139481630+geraldSinertica@users.noreply.github.com>
2024-03-22 10:10:00 -0600

    Add chages

--- DataVox/Controllers/ReportApiController.cs
+++ DataVox/Controllers/ReportApiController.cs
@@ -1,13 +1,92 @@
-﻿using System;
+﻿using DataVox.Models;
+using Newtonsoft.Json;
+using Repository.Models;
+using Services.Servicess;
+using Services.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace DataVox.Controllers
 {
+    [RoutePrefix("api/Report")]
     public class ReportApiController : ApiController
     {
+        [HttpGet]
+        [Route("Full")]
+        public async Task<IHttpActionResult> getPersonReport(string identification)
+        {
+            ResponseModel response = new ResponseModel();
+            try
+            {
+                IServiceReporte service = new ServiceReport();
+
+                Report reporte = await service.PersonReport(identification);
+
+                if (reporte == null)
+                {
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    response.Message = "Asset no encontrado verifique el id ";
+
+                }
+                else
+                {
+                    response.StatusCode = (int)HttpStatusCode.OK;
+                    response.Message = "Reporte encontrado";
+                    response.Data = reporte;
+                }
+
+                return Json(response);
+            }
+            catch (Exception e)
+            {
+
+                response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                response.Message = e.Message;
+
+                return Json(response);
+            }
+        }
+
+        [HttpGet]
+        [Route("XML")]
+        public async Task<IHttpActionResult> getPersonReportXML(string identification)
+        {
+            ResponseModel response = new ResponseModel();
+            try
+            {
+                IServiceReporte service = new ServiceReport();
+
+                Report reporte = await service.PersonReport(identification);
+
+                if (reporte == null)
+                {
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    response.Message = "Asset no encontrado verifique el id ";
+
+                }
+                else
+                {
+                    ReportToXmlConverter RC = new ReportToXmlConverter();
+                    response.StatusCode = (int)HttpStatusCode.OK;
+                    response.Message = "Reporte encontrado";
+                    response.Data = RC.ConvertJsonToXml(JsonConvert.SerializeObject(reporte));
+                }
+
+                return Json(response);
+            }
+            catch (Exception e)
+            {
+
+                response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                response.Message = e.Message;
+
+                return Json(response);
+            }
+        }
     }
 }
---
+++ DataVox/Models/ResponseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataVox.Models
+{
+    public class ResponseModel
+    {
+        public ResponseModel()
+        {
+            this.Data = null;
+            Message = string.Empty;
+            StatusCode = 0;
+        }
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public dynamic Data { get; set; }
+    }
+}
